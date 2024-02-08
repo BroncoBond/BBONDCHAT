baa1cdec894cba3a0b6cef1a8cf0fc65531e676c
Mohraiel Matta <mohraielmatta@gmail.com>
2024-02-08 03:07:37 -0800

    it works

--- bbondchat/lib/main.dart
+++ bbondchat/lib/main.dart
@@ -1,10 +1,15 @@
+//make sure to flutter run
+// if you run on 2 dif instances, you can message using socket.io
+// Import necessary packages
 import 'package:flutter/material.dart';
 import 'package:socket_io_client/socket_io_client.dart' as io;
 
+// Entry point of the application. Creates and runs the Flutter app.
 void main() {
   runApp(MyApp());
 }
 
+// Root widget of the application
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -14,43 +19,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// StatefulWidget for the chat screen
 class ChatScreen extends StatefulWidget {
   @override
   _ChatScreenState createState() => _ChatScreenState();
 }
 
+// State class for the chat screen
 class _ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = TextEditingController();
   final List<String> _messages = [];
-  io.Socket socket;
+  late io.Socket socket;
 
   @override
   void initState() {
     super.initState();
-    // Replace 'http://your-nodejs-server-ip:3000' with your Node.js server address
-    socket = io.io('http://localhost:3000', <String, dynamic>{
+    // Create a socket connection to the server
+    // Replace 'http://localhost:3000' with your actual server address
+    socket = io.io('http://localhost:50084', <String, dynamic>{
       'transports': ['websocket'],
       'autoConnect': false,
     });
 
+    // Connect to the socket
     socket.connect();
 
+    // Listen for incoming messages from the server
     socket.on('message', (data) {
+      print('Received message: $data');
+      String message = data.toString();
       setState(() {
-        _messages.add(data);
+        _messages.add(message);
       });
     });
   }
 
+  // Send a message from the current user
   void _sendMessage() {
     if (_textController.text.isNotEmpty) {
-      socket.emit('message', _textController.text);
+      String message = 'You: ${_textController.text}';
+      setState(() {
+        _messages.add(message);
+      });
+      // Emit the message to the server
+      socket.emit('message', message);
       _textController.clear();
     }
   }
 
   @override
   void dispose() {
+    // Dispose of the text controller and disconnect from the socket when the widget is disposed
     _textController.dispose();
     socket.disconnect();
     super.dispose();
@@ -60,7 +79,15 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Flutter Socket.io Chat'),
+        title: Text(
+          'Bronco Bond DM',
+          style: TextStyle(
+            fontSize: 50.0, // Set the font size to 24
+            color: Colors.green[900], // Set the color to dark green
+            fontFamily:
+                'YourCuteFont', // Replace 'YourCuteFont' with the actual font family you want to use
+          ),
+        ),
       ),
       body: Column(
         children: <Widget>[
@@ -68,8 +95,31 @@ class _ChatScreenState extends State<ChatScreen> {
             child: ListView.builder(
               itemCount: _messages.length,
               itemBuilder: (context, index) {
+                // Determine if the message belongs to the current user
+                bool isMyMessage = _messages[index].startsWith('You: ');
+
                 return ListTile(
-                  title: Text(_messages[index]),
+                  title: Align(
+                    alignment: isMyMessage
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      padding: EdgeInsets.all(8.0),
+                      decoration: BoxDecoration(
+                        color:
+                            isMyMessage ? Colors.lightGreen[900] : Colors.green,
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      child: Text(
+                        _messages[index],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  // Align messages to the right if they belong to the current user
+                  trailing: isMyMessage ? null : Icon(Icons.person),
+                  // Align messages to the left if they belong to the other person
+                  leading: isMyMessage ? Icon(Icons.person) : null,
                 );
               },
             ),
